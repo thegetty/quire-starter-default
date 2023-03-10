47a6fefe754ad10d0157fbb74a90f3241302bb8a
Anders Pollack <pollack.anders@gmail.com>
2023-03-09 16:03:32 -0800

    Implement basic click + drag interactions in `javascript/application/sequence-panel.js`

--- content/_assets/javascript/application/index.js
+++ content/_assets/javascript/application/index.js
@@ -14,7 +14,8 @@ import '../../styles/custom.css'
 // Modules (feel free to define your own and import here)
 import './canvas-panel'
 import './soundcloud-api.min.js'
-import { goToFigureState, setUpUIEventHandlers } from './canvas-panel'
+import * as canvasPanel from './canvas-panel'
+import * as sequencePanel from './sequence-panel'
 import Search from '../../../../_plugins/search/search.js'
 import scrollToHash from './scroll-to-hash'
 
@@ -190,7 +191,7 @@ function loadSearchData() {
 
 /**
  * Applies MLA format to date
- * 
+ *
  * @param  {Date}   date   javascript date object
  * @return {String}        MLA formatted date
  */
@@ -345,10 +346,11 @@ window.addEventListener('load', () => {
   scrollToHash(window.location.hash, 75, 'swing')
   const params = parseQueryParams()
   /**
-   * Canvas Panel Setup
+   * Canvas/Sequence Panel Setup
    */
-  setUpUIEventHandlers()
-  goToFigureState({
+  canvasPanel.setUpUIEventHandlers()
+  sequencePanel.setUpUIEventHandlers()
+  if (window.location.hash) canvasPanel.goToFigureState({
     figureId: window.location.hash.replace(/^#/, ''),
     annotationIds: params['annotation-id'],
     region: params['region'] ? params['region'][0] : null
---
+++ content/_assets/javascript/application/sequence-panel.js
@@ -0,0 +1,90 @@
+/**
+ * Returns a function, that, as long as it continues to be invoked, will not
+ * be triggered. The function will be called after it stops being called for
+ * N milliseconds. If `immediate` is passed, trigger the function on the
+ * leading edge, instead of the trailing.
+ *
+ * using David Waslsh's debounce
+ * https://davidwalsh.name/javascript-debounce-function
+ * which is take from underscore.js
+ *
+ * @example
+ *   var myEfficientFn = debounce(function() {
+ *     // All the taxing stuff you do
+ *   }, 250);
+ *
+ * @param {function} func - the function to execute
+ * @param {integer} wait - the tine to wait in milliseconds
+ * @param {boolean} immediate - whether to call the function immediately or at the end of the timeout
+ * @returns
+ */
+
+const debounce = function (func, wait = 250, immediate = false) {
+  let timeout = null;
+
+  return function () {
+    const context = this;
+    const args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) {
+      func.apply(context, args);
+    }
+  };
+};
+
+let oldX = 0
+
+const handleSequenceNavigation = ({ clientX, target }) => {
+  const { sequence } = target
+  const {
+    currentSequenceIndex,
+    nextCanvas,
+    previousCanvas,
+    setSequenceIndex,
+    totalCanvases
+  } = sequence
+  if (currentSequenceIndex === totalCanvases) setSequenceIndex(0)
+  if (clientX < oldX) previousCanvas()
+  if (clientX > oldX) nextCanvas()
+}
+
+const handleMouseMove = (event) => {
+  handleSequenceNavigation(event)
+  oldX = event.clientX
+}
+
+const debouncedMouseMove = debounce(handleMouseMove, 10, true)
+
+const handleMouseDown = ({ clientX }) => {
+  oldX = clientX
+  window.addEventListener('mousemove', debouncedMouseMove)
+}
+
+const handleMouseUp = () => {
+  oldX = 0
+  window.removeEventListener('mousemove', debouncedMouseMove)
+}
+
+const handleMouseLeave = () => {
+  oldX = 0
+  window.removeEventListener('mousemove', debouncedMouseMove)
+}
+
+const setUpUIEventHandlers = () => {
+  const sequencePanels = document.querySelectorAll('sequence-panel')
+  for (const sequencePanel of sequencePanels) {
+    sequencePanel.addEventListener('mousedown', handleMouseDown)
+    sequencePanel.addEventListener('mouseup', handleMouseUp)
+    sequencePanel.addEventListener('mouseleave', handleMouseLeave)
+  }
+}
+
+export { setUpUIEventHandlers }
